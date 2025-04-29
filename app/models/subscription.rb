4a91bb0e13f4b26f770ac7dd53f71a634c89ec43
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 19:17:27 -0400

    feat: add scope :with_status for filtering by status

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,6 @@ class Subscription < ApplicationRecord
 
   validates :title, :price, :frequency, :customer_id, :tea_id, presence: true
   validates :status, presence: true, inclusion: { in: ["active", "cancelled"] }
+
+  scope :with_status, ->(status) { where(status: status) }
 end
