f7a447d9a5e07c17e60cde762f411c2e96c87e48
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 12:40:14 -0400

    feat: add validations for Tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,9 @@
 class Tea < ApplicationRecord
   has_many :subscriptions
   has_many :customers, through: :subscriptions
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :temperature, presence: true
+  validates :brew_time, presence: true
 end
