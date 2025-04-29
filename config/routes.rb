e5b7729d7189e8810c988fea23691d637f8d1287
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 18:56:54 -0400

    feat: add index routes for Customers and Teas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions, only: [:index, :show, :update]
+      resources :customers, only: [:index]
+      resources :teas, only: [:index]
     end
   end
 end
