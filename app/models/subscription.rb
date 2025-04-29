8c96c0d420e8ca8e89b6ffdf3754c5594236b68b
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 12:40:03 -0400

    feat: add validations for Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true, uniqueness: true
+  validates :frequency, presence: true
+  validates :customer_id, presence: true
+  validates :tea_id, presence: true
 end
