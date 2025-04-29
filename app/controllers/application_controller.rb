2ffacfa340f851ca33b11116b9330386688fe8d2
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 16:12:51 -0400

    feat: add rescue from statements in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
-class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActionController::ParameterMissing, with: :render_bad_request
+
+private
+
+def render_not_found(error)
+  render json: { error: error.message }, status: :not_found
 end
+
+def render_bad_request(error)
+  render json: { error: error.message }, status: :bad_request
+end
+
+# TODO: Swap in ErrorSerializer if we build out custom error responses
