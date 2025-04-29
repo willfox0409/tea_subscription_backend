68962baabe516423db33901cea5aca9b4ce1d1ac
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 12:31:22 -0400

    test: write basic relationship specs for Customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { is_expected.to have_many(:subscriptions) }
+    it { is_expected.to have_many(:teas).through(:subscriptions) }
+  end
 end
+
