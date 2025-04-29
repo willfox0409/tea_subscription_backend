467bacf5c025fe5f34be6262c3627648967673ef
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 11:25:03 -0400

    feat: flatten nested Serializers with .serializable_hash[:data}, keep :id, and :type-level :data key.

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -4,10 +4,10 @@ class SubscriptionSerializer
   attributes :title, :price, :status, :frequency
 
   attribute :customer do |subscription|
-    CustomerSerializer.new(subscription.customer)
+    CustomerSerializer.new(subscription.customer).serializable_hash[:data]
   end
 
   attribute :tea do |subscription|
-    TeaSerializer.new(subscription.tea)
+    TeaSerializer.new(subscription.tea).serializable_hash[:data]
   end
 end
