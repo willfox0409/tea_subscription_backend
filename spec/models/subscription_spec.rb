68e0e72ec9ccf56bff7727e83f070fce601e8f22
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 12:32:58 -0400

    test: write basic relationship tests for Subscription

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { is_expected.to belong_to(:customer) }
+    it { is_expected.to belong_to(:tea) }
+  end
 end
