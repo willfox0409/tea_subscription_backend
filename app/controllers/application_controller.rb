eb8c7576a9737bbc4c7ca6e4da3135b0f40a0a4e
Will Fox <william.macdonald.fox@gmail.com>
2025-04-29 16:28:29 -0400

    feat: add rescue_from 422 if bad params fail model level :status validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 rescue_from ActionController::ParameterMissing, with: :render_bad_request
 
 private
@@ -9,6 +10,10 @@ end
 
 def render_bad_request(error)
   render json: { error: error.message }, status: :bad_request
+end
+
+def render_unprocessable_entity(error)
+  render json: { error: error.message }, status: :unprocessable_entity
 end
 
 # TODO: Swap in ErrorSerializer if we build out custom error responses
